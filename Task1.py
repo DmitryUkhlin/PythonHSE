87d4d2eb8b83844f5a0c4534054dd39df7b6315e
DmitryUkhlin <dmitrijuhlin359@gmail.com>
2025-03-31 00:36:55 +0700

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,25 @@
+money_capital = 20000  # Подушка безопасности
+salary = 5000  # Ежемесячная зарплата
+spend = 6000  # Траты за первый месяц
+increase = 0.05  # Ежемесячный рост цен
+value = 0
+
+# TODO Посчитайте количество  месяцев, которое можно протянуть без долгов
+
+for value in range(1, 13):
+    if value == 1:
+        money_capital = money_capital+salary-spend
+    else:
+        spend += spend*increase
+        money_capital = money_capital+salary-spend
+    if money_capital >= 0:
+        print(f"Месяц:{value}\nТекущие траты:{spend:.2f}\nТекущий бюджет на месяц:{money_capital:.2f}" )
+
+
+
+
+
+
+
+
+
